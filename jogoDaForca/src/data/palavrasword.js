904e87576ad067d29c43e6b01bf7a1344ec348e8
TM1lz <Alisson.1007697@gmail.com>
2024-12-11 09:28:21 -0300

    Config padrao do pj

---
+++ jogoDaForca/src/data/palavrasword.js
@@ -0,0 +1,22 @@
+export const wordList = {
+    carro: [
+        "Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Farol", "Bateria",
+        "Roda", "Banco", "Câmbio", "Eixo", "Rádio", "Escapamento", "Pára-brisa", "Suspensão", "Freio", "Odomêtro"
+    ],
+    fruta: [
+        "Banana", "Maçã", "Pera", "Mamão", "Laranja", "Uva", "Manga", "Melancia",
+        "Abacaxi", "Cabeludinha", "Morango", "Limão", "Abacate", "Goiaba", "Coco", "Framboesa", "Açaí", "Kiwi"
+    ],
+    corpo: [
+        "Cabeça", "Braço", "Perna", "Olho", "Boca", "Coração", "Pulmão", "Fígado",
+        "Estômago", "Intestino", "Dedo", "Cervical", "Ombro", "Coluna", "Rim", "Pele", "Nervo", "Articulação"
+    ],
+    computador: [
+        "Mouse", "Teclado", "Monitor", "Processador", "Memória", "Placa-mãe", "HD", "SSD",
+        "Fonte", "Cabos", "Placa de Vídeo", "Sistema Operacional", "Antivírus", "Browser", "Drivers", "Rede", "Internet", "GPU"
+    ],
+    trabalhos: [
+        "Advogado", "Médico", "Professor", "Engenheiro", "Arquiteto", "Dentista", "Cabeleireiro", "Motorista",
+        "Cozinheiro", "Designer", "Programador", "Empresário", "Professor", "Mecânico", "Padeiro", "Vendedor", "Enfermeiro"
+    ]
+}
