86b8b015b953694ca248551eeb7c176fe0c2c05d
TM1lz <Alisson.1007697@gmail.com>
2024-12-27 07:43:05 -0300

    tudolist MenuComp

---
+++ tudo/src/App.jsx
@@ -0,0 +1,12 @@
+import Menu from './components/menu'
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <Menu></Menu>
+    </div>
+  )
+}
+
+export default App
---
+++ tudo/src/components/Menu.jsx
@@ -0,0 +1,20 @@
+import "./Menu.css"
+
+export default function Menu() {
+  return (
+    <div className="menu">
+        <div className="container">
+            <nav>
+            <div className="search-header">
+  <input placeholder="Search" className="search-header__input" type="text" />
+
+</div>
+<button>a</button>
+            </nav>
+
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
